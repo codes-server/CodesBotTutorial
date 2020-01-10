8e4ab5a5e2677636ade8bd587692f46db8bb7d81
xRokz <38587835+rokzartz@users.noreply.github.com>
2020-01-11 02:57:10 +0300

    Delete say.js

--- commands/say.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "say",
-    alias: ["s", "echo"],
-    run: function(client, message) {
-        let args = message.content.split(" ").slice(1).join(" ")
-        message.channel.send(args);
-    }
-}
