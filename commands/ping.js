7ee99ea22d87c108b8f6ad4553e665ff8bb24036
xRokz <38587835+rokzartz@users.noreply.github.com>
2020-01-11 02:56:52 +0300

    Delete ping.js

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "ping",
-    alias: ["p", "pong"],
-    run: function(client, message) {
-        message.channel.send("Pong! " + Math.round(client.ping) + "ms")
-    }
-}
